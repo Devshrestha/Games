95d8281b6f40972fd5c00889e29ef501bd69e29f
Devansh shrestha <shrestha71devansh@gmail.com>
2020-05-02 22:48:03 +0530

    rectified player bullets

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -2,6 +2,7 @@ import pygame
 import time
 import random
 from player import player_ship
+from simple_opponent import low
 
 
 # variable used
@@ -10,17 +11,18 @@ display_height = 700
 jet_width = 70
 jet_height = 80
 back = [None]*62
-back_counter = 0
-x_position = 0
-p_bullet = []
+x_position=frame_count=b_count=back_counter = 0
+p_bullet_x = []
+p_bullet_y = []
 
 # defining colors
 black = (0, 0, 0)
 white = (255, 255, 255)
 blue = (10, 10, 250)
 yellow = (250, 250, 10)
+orange=(255,133,52)
 # initializing
-#pylint: disable=no-member
+# pylint: disable=no-member
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Spacce Invader 2150')
 clock = pygame.time.Clock()
@@ -52,29 +54,63 @@ def message_display(text, locx, locy, w, h, size, color=black):
 
 
 def soft_set():
-    pass
+    global x_position,frame_count,b_count,back_counter 
+    x_position=frame_count=b_count=back_counter = 0
+    player.draw_position_x=265
+
 
 
 def hard_set():
     pass
 
-
-def player_bullets(pos, step):
-    dis = 10
-    l = 6
+def draw_simple_opponent():
+    gameDisplay.blit(wave.img,(150,50))
+    gameDisplay.blit(wave.img,(350,50))
+    gameDisplay.blit(wave.img,(250,50))
+def player_bullets(pos, pos_y, step):
+    dis =5
+    l = 10
+  
+    for i in range(len(pos_y)):
+        pos_y[i] -= 5
+        
     player.fire()
     no = player.bullet_count
-    for i in range(len(pos)):
-        pygame.draw.line(gameDisplay, yellow,
-                         (pos[i], 600 - dis*(i+1)-step), (pos[i], 600-((i+1)*(dis+l))-step), 3)
+    if no == 1:
 
+        if len(pos)>16:
+            for i in range(len(pos)-16,len(pos)):
+                pygame.draw.line(gameDisplay, yellow,
+                         (pos[i], pos_y[i] - dis), (pos[i], pos_y[i] - dis-l), 3)
+        else:
+
+            for i in range(len(pos)):
+                pygame.draw.line(gameDisplay, yellow,
+                         (pos[i], pos_y[i] - dis), (pos[i], pos_y[i] - dis-l), 3)
+
+    if no ==2:
+        if len(pos)>16:
+            for i in range(len(pos)-16,len(pos)):
+                pygame.draw.line(gameDisplay, orange,
+                         (pos[i]-2, pos_y[i] - dis), (pos[i]-2, pos_y[i] - dis-l), 3)
+            for i in range(len(pos)-16,len(pos)):
+                pygame.draw.line(gameDisplay, orange,
+                         (pos[i]+2, pos_y[i] - dis), (pos[i]+2, pos_y[i] - dis-l), 3)
+        else:
+
+            for i in range(len(pos)):
+                pygame.draw.line(gameDisplay, orange,
+                         (pos[i]-2, pos_y[i] - dis), (pos[i]-2, pos_y[i] - dis-l), 3)
+            for i in range(len(pos)):
+                pygame.draw.line(gameDisplay, orange,
+                         (pos[i]+2, pos_y[i] - dis), (pos[i]+2, pos_y[i] - dis-l), 3)
 
 def is_colliding():
     collison = False
     if player.draw_position_x < 0:
         collison = True
-    elif player.draw_position_x > display_width-player.img_width:
-        collision = True
+    if player.draw_position_x > (display_width-player.img_width):
+        collison = True
 
     if collison:
         if player.lives < 0:
@@ -85,8 +121,8 @@ def is_colliding():
 
 
 def game_loop():
-    global back_counter, x_position, p_bullet
-    frame_count = 0
+    global back_counter, x_position, p_bullet,frame_count,b_count
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -107,17 +143,19 @@ def game_loop():
         gameDisplay.blit(back[back_counter], (0, 0))
         gameDisplay.blit(player.player_img,
                          (player.draw_position_x, player.draw_position_y))
-        if frame_count % 50 == 0:
-            p_bullet.append(
-                [[player.draw_position_x+(player.img_width)/2][600]])
-
-        #player_bullets(p_bullet, frame_count)
+        draw_simple_opponent()
+        if b_count % 8 == 0:
+            p_bullet_x.append(
+                player.draw_position_x+(player.img_width/2))
+            p_bullet_y.append(600)
+        player_bullets(p_bullet_x, p_bullet_y, frame_count)
         # create a background gif
         back_counter += 1
         if back_counter > 61:
             back_counter = 0
 
         frame_count += 1
+        b_count+=1
         # collison check for player
         is_colliding()
         pygame.display.update()
@@ -125,5 +163,6 @@ def game_loop():
 
 
 load_background()
+wave=low()
 player = player_ship()
 game_loop()
--- SpaceInvader/simple_opponent.py
+++ SpaceInvader/simple_opponent.py
@@ -1,2 +1,15 @@
 import pygame
+import random
 #pylint: disable=no-member
+
+class low():
+
+    def __init__(self):
+        self.img=pygame.image.load('resorces/simple.png')
+
+    def get_pattern(self,number):
+        pass
+
+    def spawn_wave(self):
+        self.no = random.randint(5,9)
+        self.get_pattern(self.no)
