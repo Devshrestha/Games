7cc47fb93633bb85f6983ecb8a5bc663accc2577
Devansh shrestha <shrestha71devansh@gmail.com>
2020-05-27 19:57:11 +0530

    mouse click ,flags,hidden box

--- Minesweeper/graphic.py
+++ Minesweeper/graphic.py
@@ -1,16 +1,73 @@
 import pygame
+from mine import mines
 
 
 #pylint: disable=no-member
 
 # variable used
-display_height = 630
-display_width = 600
-
+display_height = 780
+display_width = 750
 # initialising
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Mine Sweeper')
 clock = pygame.time.Clock()
+
+
+def draw_grid():
+    for i in range(values.wi):
+        for j in range(values.hi):
+            if values.grid_show[i][j] == 1:
+                if values.grid[i][j] == 0:
+                    gameDisplay.blit(values.empty, (i*50, j*50))
+                elif values.grid[i][j] == 1:
+                    gameDisplay.blit(values.one, (i*50, j*50))
+                elif values.grid[i][j] == 2:
+                    gameDisplay.blit(values.two, (i*50, j*50))
+                elif values.grid[i][j] == 3:
+                    gameDisplay.blit(values.three, (i*50, j*50))
+                elif values.grid[i][j] == 4:
+                    gameDisplay.blit(values.four, (i*50, j*50))
+                elif values.grid[i][j] == 5:
+                    gameDisplay.blit(values.five, (i*50, j*50))
+                elif values.grid[i][j] == 6:
+                    gameDisplay.blit(values.six, (i*50, j*50))
+                elif values.grid[i][j] == 7:
+                    gameDisplay.blit(values.seven, (i*50, j*50))
+                elif values.grid[i][j] == 8:
+                    gameDisplay.blit(values.eight, (i*50, j*50))
+                elif values.grid[i][j] > 8:
+                    gameDisplay.blit(values.bomb, (i*50, j*50))
+            elif values.grid_show[i][j] == 0:
+                gameDisplay.blit(values.hidden, (i*50, j*50))
+            elif values.grid_show[i][j] == 2:
+                gameDisplay.blit(values.flag, (i*50, j*50))
+
+
+def reveal_right(i, j, stage):
+    if j < 10:
+        pass
+    else:
+        return
+
+
+def mouse_press(side):
+    loc = pygame.mouse.get_pos()
+    x = loc[0]//50
+    y = loc[1]//50
+    if side == 1:
+        if values.grid_show[x][y] == 0:
+            values.grid_show[x][y] = 1
+            reveal_right(i, j, 1)
+    if side == 3:
+
+        if values.grid_show[x][y] == 0:
+            if values.no_flag > 0:
+                values.grid_show[x][y] = 2
+                values.no_flag -= 1
+        elif values.grid_show[x][y] == 2:
+            values.grid_show[x][y] = 0
+            values.no_flag += 1
+    return
 
 
 def game_loop():
@@ -19,9 +76,14 @@ def game_loop():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_press(event.button)
 
+        draw_grid()
         pygame.display.update()
         clock.tick(60)
 
+
+values = mines()
 
 game_loop()
--- Minesweeper/mine.py
+++ Minesweeper/mine.py
@@ -1,15 +1,33 @@
 import random
+import pygame
 
 
 class mines:
 
     def __init__(self):
-        self.hi = 10
-        self.wi = 10
-        self.no_mine = 10
-        self.grid = [[" " for i in range(self.wi)] for j in range(self.hi)]
-
+        self.hi = 15
+        self.wi = 15
+        self.no_mine = 15
+        self.no_flag = self.no_mine
+        self.grid = [[0 for i in range(self.wi)] for j in range(self.hi)]
+        self.grid_show = [[0 for i in range(self.wi)] for j in range(self.hi)]
+        self.load_images()
         self.generate_mine()
+
+    def load_images(self):
+        self.empty = pygame.image.load('resources/empty.png')
+        self.bomb_trig = pygame.image.load('resources/bomb_tri.jpg')
+        self.bomb = pygame.image.load('resources/bomb.jpg')
+        self.one = pygame.image.load('resources/one.png')
+        self.two = pygame.image.load('resources/two.png')
+        self.three = pygame.image.load('resources/three.png')
+        self.four = pygame.image.load('resources/four.png')
+        self.five = pygame.image.load('resources/five.png')
+        self.six = pygame.image.load('resources/six.png')
+        self.seven = pygame.image.load('resources/seven.png')
+        self.eight = pygame.image.load('resources/eight.png')
+        self.flag = pygame.image.load('resources/flag.jpg')
+        self.hidden = pygame.image.load('resources/hidden.jpg')
 
     def generate_mine(self):
         self.taken_place = []
@@ -23,7 +41,55 @@ class mines:
                 turn += 1
 
         for i in range(self.no_mine):
-            self.grid[self.taken_place[i][0]][self.taken_place[i][1]] = 'x'
+            self.grid[self.taken_place[i][0]][self.taken_place[i][1]] = 9
+
+        self.calculate_values()
+
+    def add(self, a, b):
+        try:
+
+            if a != 0 and b != 0:
+                self.grid[a-1][b-1] += 1
+        except IndexError:
+            pass
+        try:
+            if a != 0:
+                self.grid[a-1][b] += 1
+        except IndexError:
+            pass
+        try:
+            if a != 0:
+                self.grid[a-1][b+1] += 1
+        except IndexError:
+            pass
+        try:
+            if b != 0:
+                self.grid[a][b-1] += 1
+        except IndexError:
+            pass
+        try:
+            self.grid[a][b+1] += 1
+        except IndexError:
+            pass
+        try:
+            if b != 0:
+                self.grid[a+1][b-1] += 1
+        except IndexError:
+            pass
+        try:
+            self.grid[a+1][b] += 1
+        except IndexError:
+            pass
+        try:
+            self.grid[a+1][b+1] += 1
+        except IndexError:
+            pass
+
+    def calculate_values(self):
+        for i in range(self.wi):
+            for j in range(self.hi):
+                if self.grid[i][j] > 8:
+                    self.add(i, j)
 
     def print_grid(self):
         for i in range(self.wi):
